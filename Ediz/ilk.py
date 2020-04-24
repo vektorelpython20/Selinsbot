3e74c344573dc7a03dbaca4ce954ab22252be08c
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-24 21:41:33 +0300

    24042020_2141

--- Ediz/ilk.py
+++ Ediz/ilk.py
@@ -20,7 +20,11 @@ class Uygulama(QMainWindow):
     def Goster(self):
         self.btTarayici.clicked.connect(self.git)
         self.btTakip.clicked.connect(self.Takip)
+        self.btBirak.clicked.connect(self.TakipBirak)
         self.show()
+
+    def TakipBirak(self):
+        self.selinsBot.KullaniciTakipBirak(self.txtTakip.text())
 
     def Takip(self):
         self.selinsBot.KullaniciTakip(self.txtTakip.text())
@@ -36,34 +40,80 @@ class Uygulama(QMainWindow):
 
 
 class SelinsBotCore:
-    def __init__(self,username,password):
+    def __init__(self,username,password,bekle=4):
         self.username = username
         self.password = password
+        self.bekle = bekle
         self.girisYap()
     
     def girisYap(self):
         self.browser = webdriver.Firefox(executable_path=r"Driver\geckodriver.exe")
         self.browser.get("https://www.instagram.com/accounts/login")
-        time.sleep(4)
+        time.sleep(self.bekle)
         epostaGiris = self.browser.find_element_by_name("username")
         sifreGiris = self.browser.find_element_by_name("password")
         epostaGiris.send_keys(self.username)
         sifreGiris.send_keys(self.password)
         sifreGiris.send_keys(Keys.ENTER)
         #simdi değil
-        time.sleep(5)
+        time.sleep(self.bekle)
         giris = self.browser.find_element_by_css_selector("button.aOOlW:nth-child(2)")
         giris.click() 
     
     def KullaniciTakip(self,kullaniciAdi):
         self.browser.get(f"https://www.instagram.com/{kullaniciAdi}/")
-        time.sleep(4)
+        time.sleep(self.bekle)
         takipButton =  self.browser.find_element_by_css_selector("button._5f5mN:nth-child(1)")
         if (takipButton.text != ""):
             takipButton.click()
-            time.sleep(4)
+            time.sleep(self.bekle)
+        else:
+            print("Zaten Takipteyiz")
+
+
+    def KullaniciTakipBirak(self,kullaniciAdi):
+        self.browser.get(f"https://www.instagram.com/{kullaniciAdi}/")
+        time.sleep(self.bekle)
+        takipButton = self.browser.find_element_by_css_selector("button._5f5mN:nth-child(1)")
+        if (takipButton.text == ""):
+            takipButton.click()
+            time.sleep(self.bekle)
+            dogrulamaButton = self.browser.find_element_by_xpath('//button[text() = "Takibi Bırak"]')
+            dogrulamaButton.click()
+        self.browser.get(f"https://www.instagram.com/")
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = Uygulama()
     sys.exit(app.exec_())
+
+
+
+# self.browser.find_element_by_id()
+# <div id="mouseover">
+#-----------------
+# self.browser.find_element_by_class_name()
+#<div class="mouseover">
+#-----------------
+# self.browser.find_element_by_css_selector()
+# self.browser.find_element_by_css_selector("#liste > fieldset > label:nth-child(1)")
+# <div id="liste">
+#     <fieldset>
+#         <label>deneme</label>
+#         <label>deneme2</label>
+#     </fieldset>
+# </div>
+# self.browser.find_element_by_css_selector("label[data='deneme']")
+# self.browser.find_element_by_css_selector("span[id^='deneme']") # başlamalı
+# self.browser.find_element_by_css_selector("li[class$='deneme']") # bitmeli
+# self.browser.find_element_by_css_selector("li[class*='deneme']") # içermeli
+# <span name="adresbilgisi">
+# self.browser.find_element_by_css_name("adresbilgisi")
+#<footer> Benimdir </footer>
+# self.browser.find_element_by_tag("footer")
+# self.browser.find_element_partial_link_text("Ben")
+# self.browser.find_element_by_xpath("/html/body/div[1]/div[2]/div/ul/li[4]/ul/li[5]/a")
+
+
